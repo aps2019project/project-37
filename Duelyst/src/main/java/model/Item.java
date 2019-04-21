d737d63d76e65e76350a46974aa670da8d969506
Mobin Asri <mobin.asri2011@gmail.com>
2019-04-21 19:58:59 +0430

    class of Item is created.

---
+++ Duelyst/src/main/java/model/Item.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Item {
+    private long id;
+    private String name;
+    private String desc;
+
+    Item(long id, String name, String desc){
+        setName(name);
+        setId(id);
+        setDesc(desc);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void show(){
+        System.out.println(("Name : " + getName() + " Desc : " + getDesc()));
+    }
+}
