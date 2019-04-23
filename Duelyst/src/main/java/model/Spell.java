df32022da00ca420a8ce5b28ca615a929dfd965e
Mobin Asri <mobin.asri2011@gmail.com>
2019-04-23 18:46:52 +0430

    Two Constructors make it possible to make a card without ID (For Shop) and a card with ID (For Collection). Change ID from long to String and remove inGameID. Method of makeInfo is added.

--- Duelyst/src/main/java/model/Spell.java
+++ Duelyst/src/main/java/model/Spell.java
@@ -9,53 +9,52 @@ public class Spell extends Card{
     private String desc;
     private String info;
 
-    Spell(long id, long price, String name, Target target,
+    Spell(String id, Spell spell){
+        this(spell.getName(), spell.getPrice(),spell.getTarget(),
+                spell.getEffects(),spell.getMana(),spell.getDesc());
+        setId(id);
+    }
+    Spell(String name, long price, Target target,
           ArrayList<Buff> effects, int mana, String desc){
-        super(id, name, price);
+        super(name, price);
         setTarget(target);
         setEffects(effects);
         setMana(mana);
         setDesc(desc);
-
-        info = "Type : Spell - Name : " + getName();
-        info += " - MP : " + getMana() + " Description : " + getDesc();
+        makeInfo();
     }
-
     public void setTarget(Target target) {
         this.target = target;
     }
-
     public void setEffects(ArrayList<Buff> effects) {
         this.effects = effects;
     }
-
     public void setMana(int mana) {
         this.mana = mana;
     }
-
     public void setDesc(String desc){
         this.desc = desc;
     }
     public Target getTarget() {
         return target;
     }
-
-    public ArrayList<Buff> getEffect() {
+    public ArrayList<Buff> getEffects() {
         return effects;
     }
-
     public int getMana() {
         return mana;
     }
-
     public String getDesc(){
         return desc;
+    }
+    public void makeInfo(){
+        info = "Type : Spell - Name : " + getName();
+        info += " - MP : " + getMana() + " Description : " + getDesc();
     }
     @Override
     public String getInfoWithPrice() {
         return info;
     }
-
     @Override
     public String getInfoWithoutPrice() {
 
