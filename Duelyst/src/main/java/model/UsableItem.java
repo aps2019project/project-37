1b63bac63e0c19c153826b46a3f458adc08853e9
Mobin Asri <mobin.asri2011@gmail.com>
2019-04-30 14:26:01 +0430

    Make the constructors public.

--- Duelyst/src/main/java/model/UsableItem.java
+++ Duelyst/src/main/java/model/UsableItem.java
@@ -2,11 +2,11 @@ package model;
 
 public class UsableItem extends Item {
     private long price;
-    UsableItem(String name, String desc, long price){
+    public UsableItem(String name, String desc, long price){
         super(name, desc);
         setPrice(price);
     }
-    UsableItem(String id, UsableItem usable){
+    public UsableItem(String id, UsableItem usable){
         super(id, usable.getName(), usable.getDesc());
         setPrice(usable.getPrice());
     }
